7e97757f4539d29440ee5bd3f7bd6924b74c6f8f
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-02-08 14:39:34 +0300

    Add ng2-select

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,20 @@
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {SelectModule}  from 'ng2-select';
 
 import {MainComponent}   from './component/Main/main.component';
 
 @NgModule({
-    imports: [BrowserModule ],
-    declarations: [MainComponent],
-    bootstrap: [MainComponent]
+    imports: [
+        BrowserModule,
+        SelectModule
+    ],
+    declarations: [
+        MainComponent
+    ],
+    bootstrap: [
+        MainComponent
+    ]
 })
 export class AppModule {
 }
--- app/component/Main/main.component.ts
+++ app/component/Main/main.component.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core';
+import {
+    Component,
+    OnInit
+} from '@angular/core';
 
 declare var L: any;
 declare var Icon: any;
@@ -6,27 +9,35 @@ declare var Icon: any;
 @Component({
     selector: 'my-app',
     templateUrl: 'app/component/Main/main.component.html',
+    styleUrls: ['app/css/style.css']
 })
 
-export class MainComponent {
+export class MainComponent implements OnInit {
 
-  ngOnInit(){
-    document.getElementById('init_map').innerHTML = "<div id='map'></div>";
-    document.getElementById("map").style.height = window.innerHeight + "px";
+    ngOnInit() {
+        document.getElementById('init_map').innerHTML = "<div id='map'></div>";
+        document.getElementById("map").style.height = window.innerHeight + "px";
 
-    let map = L.map('map').setView([53.6834599, 23.8342648], 13);
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '',
-      maxZoom: 18
-    }).addTo(map);
+        let map = L.map('map').setView([53.6834599, 23.8342648], 13);
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '',
+            maxZoom: 18
+        }).addTo(map);
 
-    let LeafIcon = L.Icon.extend({
-      options: {
-        iconSize: [54, 54],
-        iconAnchor: [16, 37],
-        popupAnchor: [0, -30]
-      }
-    });
+        let LeafIcon = L.Icon.extend({
+            options: {
+                iconSize: [54, 54],
+                iconAnchor: [16, 37],
+                popupAnchor: [0, -30]
+            }
+        });
+    }
 
-  }
+    public types:Array<string> = ["Все объекты", "Аптека", "Клуб", "Магазин"];
+
+    private value:any = {};
+
+    public selected(value:any):void {
+        console.log('Selected value is: ', value);
+    }
 }

77f87ac60c6ef12e7f09076f0754ee1e97d31d29
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-03-17 16:46:10 +0300

    Update model

--- app/place/model/place.ts
+++ app/place/model/place.ts
@@ -2,8 +2,8 @@ export class Place {
     id_place: Number;
     name_place: string;
     description: string;
-    coordinateX: string;
-    coordinateY: string;
+    lat: string;
+    lng: string;
     address: string;
     id_type: Number;
 }
